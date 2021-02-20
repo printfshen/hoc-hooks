9719190b2b28ca7bb19f464bf40ea8d017559f98
沈 <564435314@qq.com>
2021-02-20 16:08:27 +0800

    no message

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-export { default as Test } from "hooks/useTest";
-export { default as useDocumentTitle } from "hooks/useDocumentTitle";
-export { default as useAsync } from "hooks/useAsync";
-export { default as useMount } from "hooks/useMount";
-export { default as useAsyncTable } from "hooks/useAsyncTable";
+export { default as Test } from "./hooks/useTest";
+export { default as useDocumentTitle } from "./hooks/useDocumentTitle";
+export { default as useAsync } from "./hooks/useAsync";
+export { default as useMount } from "./hooks/useMount";
+export { default as useAsyncTable } from "./hooks/useAsyncTable";
